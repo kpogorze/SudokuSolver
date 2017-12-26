72a629ee0993c9aed06356e77b0fecc751e96e98
Kamil Pogorzelski <pogorzelski.kamil@gmail.com>
2017-12-26 20:58:16 +0100

    Improve docstrings

--- src/sudoku_solver.py
+++ src/sudoku_solver.py
@@ -1,14 +1,16 @@
 import sys
+import numpy as np
 from src.sudoku import Sudoku
 
 
 class SudokuSolver(object):
-    def __init__(self, puzzle_filename, init_temp = 1, cooldown = .9999, iterations = 100000):
-        """
-        filename - path to file with sudoku puzzle
-        init_temp - base temperature
-        cooldown - temperature reduction coefficient
-        iterations - maximum number of simulated annealing loop iterations
+    def __init__(self, puzzle_filename, init_temp=1, cooldown=.9999, iterations=100000):
+        """SudokuSolver constructor
+
+        filename    - path to file with sudoku puzzle
+        init_temp   - base temperature
+        cooldown    - temperature reduction coefficient
+        iterations  - maximum number of simulated annealing loop iterations
         """
         self.temperature = init_temp
         self.cooldown = cooldown
@@ -17,29 +19,27 @@ class SudokuSolver(object):
         self.simulated_annealing()
 
     def load_data_from_file(self, puzzle_filename):
-        """
-        Loads puzzle data from file
-        """
-        data = []
-        return data
+        """Loads puzzle data from file"""
+        pass
 
     def simulated_annealing(self):
+        """Tries to solve the sudoku puzzle with simulated annealing process
+
+        At each step, algorithm generates new solution and then compares
+        values of fitness function for both new and current solution.
+        The new solution is assigned as current when its fitness is better.
+        If it's worse, discard it with certain probability based on temperature
+        and difference in scores. After each step decrease temperature by
+        small amount.
+
+        Algorithm goes on until correct solution is found or maximum number
+        of iterations is exceeded.
         """
-        Try to solve the puzzle by generating solutions deriving from current one.
-        Every time new solution is created, both current and new solutions are being
-        evaluated, and their scores compared.
-        If new score is better, replace current solution with new one. Else discard it
-        with certain probability based on temperature and difference between scores.
-        Every iteration the temperature is decreased by small amount (muultiplied by
-        cooldown coefficient).
-        End if puzzle is solved or reached maximum iterations.
-        """
+        pass
 
     def display_solution(self):
-        """
-        Displays current solution
-        """
-
+        """Displays current solution"""
+        pass
 
 
 if __name__ == "__main__":
